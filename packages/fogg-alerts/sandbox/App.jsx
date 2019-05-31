30785682414c54e81ab037c4b7e0502dd04b7ef9
Carlos <carlos.santana@dev.education>
2019-05-30 20:23:51 -0700

    Adding styled-components

--- packages/fogg-alerts/sandbox/App.jsx
+++ packages/fogg-alerts/sandbox/App.jsx
@@ -9,14 +9,14 @@ import styles from './App.scss'
 const App = () => (
   <div className={styles.App}>
     <div>
-      <Alert>This is my primary alert</Alert>
-      <Alert className="secondary">This is my secondary alert</Alert>
-      <Alert className="success">This is my success alert</Alert>
-      <Alert className="danger">This is my danger alert</Alert>
-      <Alert className="warning">This is my warning alert</Alert>
-      <Alert className="info">This is my info alert</Alert>
-      <Alert className="light">This is my light alert</Alert>
-      <Alert className="dark">This is my dark alert</Alert>
+      <Alert primary>This is my primary alert</Alert>
+      <Alert secondary>This is my secondary alert</Alert>
+      <Alert success>This is my success alert</Alert>
+      <Alert danger>This is my danger alert</Alert>
+      <Alert warning>This is my warning alert</Alert>
+      <Alert info>This is my info alert</Alert>
+      <Alert light>This is my light alert</Alert>
+      <Alert dark>This is my dark alert</Alert>
     </div>
   </div>
 )
--- packages/fogg-alerts/src/Alert.jsx
+++ packages/fogg-alerts/src/Alert.jsx
@@ -1,18 +1,68 @@
 import React from 'react'
 import { element, string } from 'prop-types'
+import styled from 'styled-components'
 
-import cx from 'classnames'
+const AlertDiv = styled.button`
+  border-radius: 0.25rem;
+  border: 1px solid transparent;
+  margin-bottom: 1rem;
+  padding: 0.75rem 1.25rem;
+  position: relative;
+  font-family: -apple-system,BlinkMacSystemFont,Roboto,"Helvetica Neue",Arial,sans-serif;
+  font-size: 1rem;
+  font-weight: 400;
+  color: #004085;
+  background-color: #cce5ff;
+  border-color: #b8daff;
 
-import styles from './Alert.scss'
+  ${({ secondary }) => secondary && `
+    color: #383d41;
+    background-color: #e2e3e5;
+    border-color: #d6d8db;
+  `}
+
+  ${({ success }) => success && `
+    color: #155724;
+    background-color: #d4edda;
+    border-color: #c3e6cb;
+  `}
+
+  ${({ danger }) => danger && `
+    color: #721c24;
+    background-color: #f8d7da;
+    border-color: #f5c6cb;
+  `}
+
+  ${({ info }) => info && `
+    color: #0c5460;
+    background-color: #d1ecf1;
+    border-color: #bee5eb;
+  `}
+
+  ${({ light }) => light && `
+    color: #7f7f81;
+    background-color: #fdfdfe;
+    border-color: #fcfcfd;
+  `}
+
+  ${({ dark }) => dark && `
+    color: #1f2533;
+    background-color: #d8dae0;
+    border-color: #c8ccd3;
+  `}
+
+  ${({ warning }) => warning && `
+    color: #856404;
+    background-color: #fff3cd;
+    border-color: #ffeeba;
+  `}
+`
+
 
 const Alert = props => {
-  const { children, className = 'primary' } = props
+  const { children } = props
 
-  return (
-    <div className={cx(styles.foggAlert, styles[className])} role="alert">
-      {children}
-    </div>
-  )
+  return <AlertDiv {...props}>{children}</AlertDiv>
 }
 
 Alert.propTypes = {
