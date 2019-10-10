8239e09d44e45cf23fd9845b8dcb80880252437d
Carlos <carlos.santana@dev.education>
2019-10-09 23:46:18 -0700

    Adding Center component

--- packages/fogg-ui/src/entry.js
+++ packages/fogg-ui/src/entry.js
@@ -1,6 +1,7 @@
 export { default as Accordion } from './Accordion'
 export { default as Alert } from './Alert'
 export { default as Button } from './Button'
+export { default as Center } from './Center'
 export { default as Checkbox } from './Checkbox'
 export { default as Icon } from './Icon'
 export { default as Input } from './Input'
