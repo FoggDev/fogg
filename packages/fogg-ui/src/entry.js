1d1499d17ae6a03d4f7a6d115ddb7312610d1417
Carlos <carlos.santana@dev.education>
2019-10-17 19:38:26 -0700

    Exporting Badge

--- packages/fogg-ui/src/entry.js
+++ packages/fogg-ui/src/entry.js
@@ -1,5 +1,6 @@
 export { default as Accordion } from './Accordion'
 export { default as Alert } from './Alert'
+export { default as Badge } from './Badge'
 export { default as Button } from './Button'
 export { default as Center } from './Center'
 export { default as Checkbox } from './Checkbox'
