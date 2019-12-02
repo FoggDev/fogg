e405617bdded944471d9ebe568535e80f31c0c98
Carlos Santana <carlos.santana@dev.education>
2019-12-01 23:06:09 -0800

    fogg-utils: Adding frontend utils

---
+++ packages/fogg-utils/src/frontend/index.js
@@ -0,0 +1,18 @@
+// Utils
+import { isBrowser, isDefined } from '../is'
+
+export function cx(...classes) {
+  return classes.join(' ').trim()
+}
+
+export function redirectTo(url = '/') {
+  if (isBrowser()) {
+    window.location.href = url
+  }
+
+  return false
+}
+
+export function isFirstRender(items) {
+  return !isDefined(items) || items.length === 0 || Object.keys(items).length === 0
+}
--- packages/fogg-utils/src/index.js
+++ packages/fogg-utils/src/index.js
@@ -1,2 +1,3 @@
 export * from './is'
 export * from './security'
+export * from './frontend'
