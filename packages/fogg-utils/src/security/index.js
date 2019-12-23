5d88b2a19ea220ca4f6459fcd86965f48a629576
Carlos Santana <carlos.santana@dev.education>
2019-12-23 14:04:31 -0800

    fogg-utils: Adding getRandomCode

--- packages/fogg-utils/src/security/index.js
+++ packages/fogg-utils/src/security/index.js
@@ -34,3 +34,18 @@ export function setBase64(value) {
 
   return false
 }
+
+export function getRandomCode(max) {
+  let code = ''
+  let randomPoz
+  const charset = 'ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789'
+
+  max = max || 12
+
+  for (let i = 0; i < max; i += 1) {
+    randomPoz = Math.floor(Math.random() * charset.length)
+    code += charset.substring(randomPoz, randomPoz + 1)
+  }
+
+  return code
+}
