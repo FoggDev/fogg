e0780dc8ac829502a0033fae27f27d106c233338
Carlos Santana <carlos.santana@dev.education>
2019-12-28 11:33:18 -0800

    fogg-utils: Adding validateFields

--- packages/fogg-utils/src/graphql/index.js
+++ packages/fogg-utils/src/graphql/index.js
@@ -4,3 +4,37 @@ export const getGraphQlError = error => {
     message: error.toString().replace('Error: GraphQL error: ', '')
   }
 }
+
+export const validateFields = (validations, values, alert = 'Error trying to create the item') => {
+  const messages = {}
+
+  Object.keys(validations).forEach(field => {
+    const { len, isEmpty } = validations[field]
+
+    if (len && len.arg) {
+      if (values[field].length !== len.arg) {
+        messages[field] = {
+          msg: len.msg
+        }
+      }
+    }
+
+    if (isEmpty) {
+      if (values[field] === '' || !values[field]) {
+        messages[field] = {
+          msg: isEmpty.msg
+        }
+      }
+    }
+  })
+
+  return Object.keys(messages).length > 0
+    ? {
+      error: true,
+      alert,
+      messages
+    }
+    : {
+      error: false
+    }
+}
